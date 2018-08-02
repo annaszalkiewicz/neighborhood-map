e3adb9cd4223daa03f3d0be7f125f64baa9a08e2
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-08-02 22:04:46 +0200

    Fix map visibility

--- src/js/App.js
+++ src/js/App.js
@@ -18,18 +18,71 @@ class App extends Component {
 	}
 
 	initMap = () => {
-		const centerMap = {lat: -25.344, lng: 131.036};
+
+		const styles = [
+			{
+				"featureType": "all",
+				"elementType": "all",
+				"stylers": [
+					{
+						"hue": "#1E4C40"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "all",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "road",
+				"elementType": "all",
+				"stylers": [
+					{
+						"saturation": -70
+					}
+				]
+			},
+			{
+				"featureType": "transit",
+				"elementType": "all",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "all",
+				"stylers": [
+					{
+						"visibility": "simplified"
+					},
+					{
+						"saturation": -60
+					}
+				]
+			}
+		];
 
 		let map = new window.google.maps.Map( 
 			document.getElementById('map'), {
-				zoom: 4, 
-				center: centerMap
+				zoom: 6, 
+				center: {lat: 51.7730971, lng: 19.4105533} ,
+				styles: styles,
+				mapTypeControl: false,
 		});
-		this.setState({map: map})
 	}
 	render() {
 		return (
-			<div ref="map" id="map" className="map" role="application"></div>
+			<main>
+				<div ref="map" id="map" className="map" role="application"></div>
+			</main>
 		);
 	}
 }
